a27b4d82d5e9f339ccf892da618d8282d24cab3b
HarikaPatro <patroharika866@gmail.com>
2026-05-29 19:42:07 +0530

    sg

--- 10-sg/locals.tf
+++ 10-sg/locals.tf
@@ -1,3 +1,4 @@
 local {
     common_name_suffix = "${var.project_name}-${var.environment}" #roboshop-dev
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
 }
--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -8,3 +8,14 @@
   vpc_id      = data.aws_ssm_parameter.vpc_id.value
 
 } */
+
+module "sg" {
+    count = length(var.sg_names)
+    source = "git::https://github.com/HarikaPatro/terraform-aws-sg.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = var.sg_names[count.index]
+    sg_description = "created for ${var.sg_names[count.index]}"
+    vpc_id = local.vpc_id
+
+}
