48851575967a8464fb70dc8496fc29766f356d1e
HarikaPatro <patroharika866@gmail.com>
2026-05-29 19:40:24 +0530

    bastion

---
+++ 20-bastion/locals.tf
@@ -0,0 +1,10 @@
+locals {
+    ami_id = data.aws_ami.joindevops.id
+    bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+    common_tags = {
+        project_name = var.project_name
+        environment = var.environment
+        Terraform = true
+    }
+    
+}
