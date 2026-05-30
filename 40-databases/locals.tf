4f973ace6db2561a2f1aa24480ea4d2a996be6bc
HarikaPatro <patroharika866@gmail.com>
2026-05-30 14:02:22 +0530

    sgrules

---
+++ 40-databases/locals.tf
@@ -0,0 +1,11 @@
+locals {
+    ami_id = data.aws_ami.joindevops.id
+    mongodb_sg_id = data.aws_ssm_parameter.mongodbn_sg_id.value
+    database_subnet_id = split("," , data.aws_ssm_parameter.database_subnet_ids.value)[0]
+    common_tags = {
+        project_name = var.project_name
+        environment = var.environment
+        Terraform = true
+    }
+    
+}
