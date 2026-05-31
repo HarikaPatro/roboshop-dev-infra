3332f9ddb219db35675ede9c00c6f101d7ec138e
HarikaPatro <patroharika866@gmail.com>
2026-05-31 13:59:22 +0530

    sgrules

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -130,6 +130,7 @@ resource "aws_instance" "mysql" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.mysql_sg_id]
   subnet_id = local.database_subnet_id
+  iam_instance_profile = aws_iam_instance_profile.mysql.name
 
   tags = merge(
        local.common_tags,
@@ -139,6 +140,11 @@ resource "aws_instance" "mysql" {
   )
   
 } 
+
+resource "aws_iam_instance_profile" "mysql" {
+  name = "mysql"
+  role = "EC2SSMParameterRead"
+}
 
 resource "terraform_data" "mysql" {
   triggers_replace = [
@@ -161,7 +167,7 @@ resource "terraform_data" "mysql" {
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh mysql"
+      "sudo sh /tmp/bootstrap.sh mysql dev"
     ]
   }
 }
