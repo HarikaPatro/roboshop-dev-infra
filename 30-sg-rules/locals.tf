8bad594d6e54371c91203d4e083477f344f1588f
HarikaPatro <patroharika866@gmail.com>
2026-05-31 17:04:00 +0530

    sgrules

--- 30-sg-rules/locals.tf
+++ 30-sg-rules/locals.tf
@@ -5,4 +5,5 @@ locals {
     redis_sg_id = data.aws_ssm_parameter.redis_sg_id
     rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id
     mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id
 }
