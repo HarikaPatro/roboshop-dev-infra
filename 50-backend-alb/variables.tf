1605a56f24d3b2fcb639500e5608a93db3cf1205
HarikaPatro <patroharika866@gmail.com>
2026-06-01 20:35:02 +0530

    sgrules

--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z00144233DSWXIB51KLPM"
+}
+
+variable "domain_name" {
+    default = "daws87s.fun"
+}
