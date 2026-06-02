973d6e3a78418c3f1bd762a2d0a5bd55e824adf0
HarikaPatro <patroharika866@gmail.com>
2026-06-02 14:02:33 +0530

    sgrules

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z00144233DSWXIB51KLPM"
+}
+
+variable "domain_name" {
+    default = "daws87s.fun"
+}
